4d3288965db9e1dfc37d2063f1f2d7dbdd974d07
satish rajput <satishetc4288@gmail.com>
2018-12-03 21:29:33 +0530

    initial commint man

---
+++ src/main/java/com/satish/exp/Application.java
@@ -0,0 +1,8 @@
+package com.satish.exp;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("hell world, i am programming in spring boot here");
+    }
+}
