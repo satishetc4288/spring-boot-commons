079aca758eee938449ea4fb47c8fdfe3eeb57480
satish rajput <satishetc4288@gmail.com>
2024-03-02 20:39:36 +0530

    fix

---
+++ src/main/java/com/satish/exp/problems/LongestRepCharacter.java
@@ -0,0 +1,23 @@
+package com.satish.exp.problems;
+
+public class LongestRepCharacter {
+    public static void main(String[] args) {
+        String str = "gasakkmmaajgsiuarrrrhsjbaskja";
+    }
+    private static void printLongestRepCharacter(String str){
+        Integer current = 0;
+        Integer last = 0;
+        for(int i=0; i<str.length()-1; i++){
+            if(str.charAt(i) == str.charAt(i+1))
+                current++;
+            else if(current > last){
+                last = current;
+                current=0;
+            }
+        }
+        if(current > last)
+            System.out.println(current);
+        else
+            System.out.println(last);
+    }
+}
--- src/main/java/com/satish/exp/problems/LongestSubString.java
+++
@@ -1,7 +0,0 @@
-package com.satish.exp.problems;
-
-public class LongestSubString {
-    public static void main(String[] args) {
-
-    }
-}
